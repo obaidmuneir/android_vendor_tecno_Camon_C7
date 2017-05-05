cd8fa2e4b995d1c37efa0654de447242601b55ed
iykequame <nanaquameallday@gmail.com>
2017-05-05 02:14:53 +0000

    committed by : iyke

---
+++ Camon_C7-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon_C7/vendor,system/vendor)
